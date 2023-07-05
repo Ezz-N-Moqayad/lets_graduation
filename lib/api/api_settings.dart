d17aa3be0a3cd2480646ac9eaf7f6cf11cb39969
Ezz-N-Moqayad <ezz.n.moq@gmail.com>
2023-07-05 18:42:14 +0300

    android: 7.5.1

---
+++ lib/api/api_settings.dart
@@ -0,0 +1,9 @@
+class ApiSettings {
+  static const _BASE_URL = 'https://khader.atyaf.cloud/api/';
+  static const LOGIN = '${_BASE_URL}login';
+  static const REGISTER = '${_BASE_URL}register';
+  static const MALE_VIDEO = '${_BASE_URL}post/get/male';
+  static const FEMALE_VIDEO = '${_BASE_URL}post/get/female';
+  static const HEALTH= '${_BASE_URL}food/show/1';
+  static const GYM = '${_BASE_URL}gym/show/1';
+}
