ac4f2aa0b2771793954d33a5f4fcc32b65ca7414
Ezz-N-Moqayad <ezz.n.moq@gmail.com>
2023-07-05 18:41:10 +0300

    android: 7.5.1

--- lib/common/entities/fb_response.dart
+++
@@ -1,9 +0,0 @@
-class FbResponse {
-  final String message;
-  final bool states;
-
-  FbResponse({
-    required this.message,
-    required this.states,
-  });
-}
--- lib/common/langs/en_US.dart
+++
@@ -1,4 +0,0 @@
-Map<String, String> en_US = {
-  'title': 'This is Title!',
-  'login': 'logged in as @name with email @email',
-};
